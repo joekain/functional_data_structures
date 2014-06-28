9024571143e99690351dcf424dfb5822c1535e30
Joseph Kain <joekain@gmail.com>
2014-06-28 09:06:19 -0700

    Exercises 2-6

--- lib/functional_data_structures.ex
+++ lib/functional_data_structures.ex
@@ -1,3 +1,25 @@
 defmodule FunctionalDataStructures do
-  
+  def tail([]), do: []
+  def tail([_h | t]), do: t
+
+  def setHead([], _newHead), do: []
+  def setHead([_h | t], newHead), do: [newHead | t]
+
+  def drop([], 2), do: []
+  def drop(l, 0), do: l
+  def drop([_h | t], n), do: drop(t, n - 1)
+
+  def dropWhile([], _f), do: []
+  def dropWhile([h | t], f) do
+    case f.(h) do
+      true -> dropWhile(t, f)
+      false -> [h | t]
+    end
+  end
+
+  # This runs in time proportional to the lenght of the list
+  # because the only way to find the end of the list is to traverse.
+  def initial([]), do: []
+  def initial([h | []]), do: []
+  def initial([h | t]), do: [h | initial(t)]
 end
--- test/functional_data_structures_test.exs
+++ test/functional_data_structures_test.exs
@@ -1,7 +1,43 @@
+
+# Based on the exercises in Chapter 3 of _Functional Programming in Scala_
 defmodule FunctionalDataStructuresTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  alias FunctionalDataStructures, as: L
+
+  test "tail of empty list" do
+    assert L.tail([]) == []
+  end
+
+  test "tail of normal list" do
+    assert L.tail([1, 2, 3, 4]) == [2, 3, 4]
+  end
+
+  test "setHead of empty list" do
+    assert L.setHead([], 5) == []
+  end
+
+  test "setHead of normal list should replace head of list" do
+    assert L.setHead([1, 2, 3, 4], 5) == [5, 2, 3, 4]
+  end
+
+  test "drop of empty list" do
+    assert L.drop([], 2) == []
+  end
+
+  test "drop of 0 elements should be same list" do
+    assert L.drop([1, 2, 3], 0) == [1, 2, 3]
+  end
+
+  test "drop of normal list shold remove first n elements of the list" do
+    assert L.drop([1, 2, 3, 4, 5], 2) == [3, 4, 5]
+  end
+
+  test "dropWhile removes elements from list while they match a predicate" do
+    assert L.dropWhile([1, 2, 3, 4], &(&1 < 3)) == [3, 4]
+  end
+
+  test "initial returns a list will all elements except the last of a list" do
+    assert L.initial([1, 2, 3, 4]) == [1, 2, 3]
   end
 end
