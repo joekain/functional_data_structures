7d8b3ef97ce0567bce3d816fd872e7c8087b36b1
Joseph Kain <joekain@gmail.com>
2014-06-28 11:08:38 -0700

    Exercises 7-15

--- lib/functional_data_structures.ex
+++ lib/functional_data_structures.ex
@@ -20,6 +20,37 @@ defmodule FunctionalDataStructures do
   # This runs in time proportional to the lenght of the list
   # because the only way to find the end of the list is to traverse.
   def initial([]), do: []
-  def initial([h | []]), do: []
+  def initial([_h | []]), do: []
   def initial([h | t]), do: [h | initial(t)]
+
+  def length(l), do: List.foldr(l, 0, fn (_x, acc) -> acc + 1 end)
+
+  def foldLeft([], acc, _f), do: acc
+  def foldLeft([h | t], acc, f), do: foldLeft(t, f.(h, acc), f)
+
+  def sumLeft(l), do: foldLeft(l, 0, &(&1 + &2))
+  def productLeft(l), do: foldLeft(l, 1, &(&1 * &2))
+  def lengthLeft(l), do: foldLeft(l, 0, fn (_x, acc) -> acc + 1 end)
+
+  def reverse(l), do: foldLeft(l, [], fn (x, acc) -> [x | acc] end)
+
+  # The chain_accumulator passed into foldr is a function which means the
+  # foldr produces a chain of functions.  For input [x, y, z] it
+  # would produce:
+  #   fn (b) -> f.x(f.y(f.z(b))) end
+  # Then we evaluate this chain at the original acc value to get the final
+  # result.
+  def foldLeftUsingRight(l, acc, f) do
+    base = fn (b) -> b end
+    chain = List.foldr(l, base,
+      fn (x, accumulator) -> (fn (b) -> accumulator.(f.(x, b)) end) end)
+    chain.(acc)
+  end
+
+  def foldRightUsingLeft(l, acc, f), do: foldLeft(reverse(l), acc, f)
+
+  def append(l, m), do: List.foldr(l, m, &([&1 | &2]))
+
+  def concat([]), do: []
+  def concat([head_list | tail]), do: append(head_list, concat(tail))
 end
--- test/functional_data_structures_test.exs
+++ test/functional_data_structures_test.exs
@@ -40,4 +40,44 @@ defmodule FunctionalDataStructuresTest do
   test "initial returns a list will all elements except the last of a list" do
     assert L.initial([1, 2, 3, 4]) == [1, 2, 3]
   end
+
+  test "length computes the length of a list" do
+    assert L.length([1, 2, 3, 4]) == 4
+  end
+
+  test "foldLeft of a normal list" do
+    assert L.foldLeft([1, 2, 3, 4], 0, &(&1 + &2)) == 10
+  end
+
+  test "sum using foldLeft" do
+    assert L.sumLeft([1, 2, 3, 4]) == 10
+  end
+
+  test "product using foldLeft" do
+    assert L.productLeft([1, 2, 3, 4]) == 24
+  end
+
+  test "length using foldLeft" do
+    assert L.lengthLeft([1, 2, 3, 4]) == 4
+  end
+
+  test "reverse a list" do
+    assert L.reverse([1, 2, 3, 4]) == [4, 3, 2, 1]
+  end
+
+  test "foldLeft written using foldRight" do
+    assert L.foldLeftUsingRight([1, 2, 3, 4], 0, &(&1 - &2)) == List.foldl([1, 2, 3, 4], 0, &(&1 - &2))
+  end
+
+  test "foldRight written using foldLeft" do
+    assert L.foldRightUsingLeft([1, 2, 3, 4], 0, &(&1 - &2)) == List.foldr([1, 2, 3, 4], 0, &(&1 - &2))
+  end
+
+  test "append" do
+    assert L.append([1, 2, 3, 4], [8, 9, 10]) == [1, 2, 3, 4, 8, 9, 10]
+  end
+
+  test "concat list of lists into a single list" do
+    assert L.concat([[1,2],[3],[],[4,5,6]]) == [1,2,3,4,5,6]
+  end
 end
